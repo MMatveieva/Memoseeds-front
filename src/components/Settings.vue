b992597a4b2e316f99a22f5f672f4aea8e7f34df
naumchyk <andriy.nau@gmail.com>
2019-04-08 16:33:52 +0300

    Update Settings.vue

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -281,13 +281,13 @@
          return new Promise((resolve, reject) => {
            const reader = new FileReader();
            reader.readAsDataURL(file);
-           reader.onload = () => {
+           reader.addEventListener("loadend", function() {
              let encoded = reader.result.replace(/^data:(.*;base64,)?/, '');
              if ((encoded.length % 4) > 0) {
                encoded += '='.repeat(4 - (encoded.length % 4));
              }
              resolve(encoded);
-           };
+           });
            reader.onerror = error => reject(error);
          });
        }
